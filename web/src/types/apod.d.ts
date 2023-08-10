875659f32f39d8d2af08f42272e7c30459fa558c
Berk Cambaz <berkcambaz12321@gmail.com>
2023-08-10 18:43:25 +0300

    Implement Picture of the Day route

---
+++ web/src/types/apod.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Astronomy picture of the day.
+ */
+export interface IAPOD {
+  date: string
+  explanation: string
+  hdurl: string
+  url: string
+  media_type: string
+  service_version: string
+  title: string
+}
--- web/src/views/PictureOfTheDay.vue
+++ web/src/views/PictureOfTheDay.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useDisplay } from 'vuetify';
+import type { IAPOD } from '@/types/apod'
 
+const display = useDisplay();
+
+const apod = ref<IAPOD | undefined>(undefined);
+
+onMounted(async () => {
+  try {
+    const result = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+    const json = await result.json() as IAPOD;
+    apod.value = json;
+  } catch (error) {
+
+  }
+})
 </script>
 
 <template>
-  hello, world!
+  <VSheet class="mx-4 my-4">
+    <VImg height="calc(75vh - 64px)" :src="apod?.url" />
+    <VSheet :max-width="display.thresholds.value.sm" class="mx-auto">
+      <p class="text-h4 text-center mt-4 mb-2">{{ apod?.title }}</p>
+      <h2 class="text-h4 text-center mt-4 mb-2">{{ apod?.title }}</h2>
+      <p class="text-body-1 text-center">{{ apod?.explanation }}</p>
+    </VSheet>
+  </VSheet>
 </template>
