87ef180c6637e86e31790b234700d12d7bd014c6
Berk Cambaz <berkcambaz12321@gmail.com>
2023-08-10 20:37:34 +0300

    Make app bar more responsive & hide/show back button

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { computed } from 'vue';
+import type { StyleValue } from 'vue';
 import { RouterView, useRouter } from 'vue-router'
 import { useDisplay } from "vuetify";
 
 const display = useDisplay();
 const router = useRouter();
 
+const backButtonStyle = computed((): StyleValue => router.currentRoute.value.name === "home" ? { visibility: "hidden" } : {})
 const pictureOfTheDayClass = computed(() => ({ "text-blue-lighten-1": router.currentRoute.value.name === "picture-of-the-day" }))
-const youOnEarthdClass = computed(() => ({ "text-blue-lighten-1": router.currentRoute.value.name === "you-on-earth" }))
+const youOnEarthClass = computed(() => ({ "text-blue-lighten-1": router.currentRoute.value.name === "you-on-earth" }))
 const earthEnhancedClass = computed(() => ({ "text-blue-lighten-1": router.currentRoute.value.name === "earth-enhanced" }))
 
 </script>
@@ -16,29 +18,38 @@ const earthEnhancedClass = computed(() => ({ "text-blue-lighten-1": router.curre
   <VLayout>
     <VAppBar>
       <VSheet class="ma-auto d-flex flex-1-1 flex-row justify-space-between" :max-width="display.thresholds.value.md">
-        <VSheet class="d-flex align-center">
-          <VBtn icon="mdi-arrow-left" @click="router.go(-1)" />
 
-          <RouterLink to="/home" class="text-decoration-none text-light-blue">
+        <VSheet class="d-flex align-center">
+          <VBtn :style="backButtonStyle" class="v-hidden ma-1" icon="mdi-arrow-left" @click="router.go(-1)" />
+
+          <RouterLink to="/home" class="ma-1 text-decoration-none text-light-blue">
             <h2>Space Explorer</h2>
           </RouterLink>
         </VSheet>
 
-        <VSheet class="d-flex align-center">
-          <VBtn color="blue" variant="elevated">Login</VBtn>
+        <VSheet v-if="display.smAndUp.value" class="d-flex align-center">
+          <VBtn class="ma-1" color="blue" variant="elevated" prepend-icon="mdi-login">Login</VBtn>
 
-          <RouterLink to="/picture-of-the-day">
-            <VBtn variant="flat" :class="pictureOfTheDayClass">Picture of the Day</VBtn>
+          <RouterLink to="/picture-of-the-day" class="ma-1">
+            <VBtn v-if="display.mdAndUp.value" variant="flat" :class="pictureOfTheDayClass">Picture of the Day</VBtn>
+            <VBtn v-else icon="mdi-earth" variant="flat" :class="pictureOfTheDayClass" />
           </RouterLink>
 
-          <RouterLink to="/you-on-earth">
-            <VBtn variant="flat" :class="youOnEarthdClass">You on Earth</VBtn>
+          <RouterLink to="/you-on-earth" class="ma-1">
+            <VBtn v-if="display.mdAndUp.value" variant="flat" :class="youOnEarthClass">You on Earth</VBtn>
+            <VBtn v-else icon="mdi-map-marker" variant="flat" :class="youOnEarthClass" />
           </RouterLink>
 
-          <RouterLink to="/earth-enhanced">
-            <VBtn variant="flat" :class="earthEnhancedClass">Earth Enhanced</VBtn>
+          <RouterLink to="/earth-enhanced" class="ma-1">
+            <VBtn v-if="display.mdAndUp.value" variant="flat" :class="earthEnhancedClass">Earth Enhanced</VBtn>
+            <VBtn v-else icon="mdi-earth-plus" variant="flat" :class="earthEnhancedClass" />
           </RouterLink>
         </VSheet>
+
+        <VSheet v-else>
+          <VBtn icon="mdi-menu" class="ma-1" />
+        </VSheet>
+
       </VSheet>
     </VAppBar>
 
