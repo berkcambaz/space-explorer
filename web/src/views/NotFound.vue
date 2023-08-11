e6cf002098ee9caed17b1f51527fb39383fe4979
Berk Cambaz <berkcambaz12321@gmail.com>
2023-08-11 20:55:46 +0300

    Implement not found route

--- web/src/views/NotFound.vue
+++ web/src/views/NotFound.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
+import undrawTaken from '@/assets/undraw_taken.svg'
+import { useDisplay } from 'vuetify';
 
+const display = useDisplay();
 </script>
 
 <template>
-  not found!
+  <VSheet :max-width="display.thresholds.value.sm" class="mx-auto my-4 bg-transparent">
+    <VImg class="mx-auto" :src="undrawTaken" maxWidth="300" />
+
+    <div class="text-center my-2">
+      <h1 class="text-h3">404</h1>
+      <p class="text-body-1">Sorry, couldn't find what you are looking for.</p>
+    </div>
+
+    <VSheet class="d-flex justify-center bg-transparent">
+      <RouterLink to="/home">
+        <VBtn variant="flat">Go to Home</VBtn>
+      </RouterLink>
+    </VSheet>
+  </VSheet>
 </template>
