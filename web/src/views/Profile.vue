b12bc70e3c8cff50bc2689afe2dca1c85e101750
Berk Cambaz <berkcambaz12321@gmail.com>
2023-08-13 20:39:56 +0300

    Fetch user related data on profile route

--- web/src/views/Profile.vue
+++ web/src/views/Profile.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts">
+import { useAppStore } from '@/stores/app';
+import type { IFavourite } from '@/types/favourite';
+import type { IUser } from '@/types/user';
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
+const appStore = useAppStore();
+
+const user = ref<IUser | undefined>(undefined);
+const favourites = ref<IFavourite[] | undefined>(undefined);
+
+onMounted(async () => {
+  const userId = router.currentRoute.value.params.id as string;
+
+  const [_user, _favourites] = await Promise.all([
+    appStore.getUser(userId),
+    appStore.getUserAllFavouriteImages(userId),
+  ]);
+
+  user.value = _user;
+  favourites.value = _favourites;
+})
 </script>
 
 <template>
-  hello, world!
+  {{ user?.id }}
 </template>
