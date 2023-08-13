66bb92c93bdb4975aa2673b654b441cb514e2577
Berk Cambaz <berkcambaz12321@gmail.com>
2023-08-13 21:51:55 +0300

    Implement profile route

--- web/src/views/Profile.vue
+++ web/src/views/Profile.vue
@@ -1,16 +1,39 @@
 <script setup lang="ts">
+import { util } from '@/lib/util';
 import { useAppStore } from '@/stores/app';
 import type { IFavourite } from '@/types/favourite';
 import type { IUser } from '@/types/user';
 import { ref } from 'vue';
+import { computed } from 'vue';
 import { onMounted } from 'vue';
 import { useRouter } from 'vue-router';
+import Progress from "@/components/Progress.vue"
+import { useDisplay } from 'vuetify';
 
+const display = useDisplay();
 const router = useRouter();
 const appStore = useAppStore();
 
 const user = ref<IUser | undefined>(undefined);
-const favourites = ref<IFavourite[] | undefined>(undefined);
+const favourites = ref<IFavourite[]>([]);
+
+const favouritesShown = ref<IFavourite[]>([]);
+const loading = ref(true);
+
+const profileName = computed(() => user.value?.name || "Space Explorer")
+const profileBio = computed(() => user.value?.bio || "Bio of the Space Explorer")
+const profileDate = computed(
+  () => user.value ? util.formatDate(new Date(user.value.created_at)) : "Their journey started at..."
+)
+const profileImageSrc = computed(() => user.value?.profile_image || "/android-chrome-512x512.png")
+
+function loadMore() {
+  const diff = Math.min(favourites.value.length - favouritesShown.value.length, 3);
+  if (diff <= 0) return;
+
+  const currentLength = favouritesShown.value.length;
+  favouritesShown.value.push(...favourites.value.slice(currentLength, currentLength + diff));
+}
 
 onMounted(async () => {
   const userId = router.currentRoute.value.params.id as string;
@@ -22,9 +45,46 @@ onMounted(async () => {
 
   user.value = _user;
   favourites.value = _favourites;
+
+  favouritesShown.value = _favourites.slice(0, 3);
+  loading.value = false;
 })
+
 </script>
 
 <template>
-  {{ user?.id }}
+  <VSheet class="ma-4 bg-transparent">
+    <VCard class="mx-auto" :max-width="display.thresholds.value.md">
+      <VContainer class="d-flex justify-space-between">
+        <VAvatar :image="profileImageSrc" size="128" />
+
+        <VBtn icon="mdi-pencil-outline" />
+      </VContainer>
+
+      <VContainer>
+        <h1 class="text-h5">{{ profileName }}</h1>
+        <p class="text-body-1 my-2">{{ profileBio }}</p>
+        <div class="d-flex align-center">
+          <VIcon icon="mdi-calendar" />
+          <p class="text-body-1 mx-2">{{ profileDate }}</p>
+        </div>
+      </VContainer>
+
+      <VOverlay v-model="loading" persistent contained class="align-center justify-center"><Progress /></VOverlay>
+    </VCard>
+  </VSheet>
+
+  <VSheet class="mx-auto my-4 px-4 bg-transparent" :max-width="display.thresholds.value.md">
+    <VRow>
+      <VCol v-for="data in favouritesShown" :key="data.id" cols="4">
+        <VImg :src="data.image_url" aspect-ratio="1" cover>
+          <template v-slot:placeholder><Progress /></template>
+        </VImg>
+      </VCol>
+    </VRow>
+
+    <div class="d-flex align-center my-4">
+      <VBtn class="mx-auto" @click="loadMore">Load More</VBtn>
+    </div>
+  </VSheet>
 </template>
