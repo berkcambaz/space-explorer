1fc9419c9a506c9526f83c223473ac7026937392
Berk Cambaz <berkcambaz12321@gmail.com>
2023-08-13 20:39:04 +0300

    Implement createUser and getUser features to app store

--- web/src/stores/app.ts
+++ web/src/stores/app.ts
@@ -3,6 +3,7 @@ import type { Session } from "@supabase/supabase-js";
 import type { IUser } from '@/types/user';
 import type { IFavourite } from '@/types/favourite';
 import { supabase } from '@/lib/supabase';
+import { th } from 'vuetify/locale';
 
 export const useAppStore = defineStore('app', {
   state: () => ({
@@ -22,15 +23,16 @@ export const useAppStore = defineStore('app', {
     }
   },
   actions: {
-    async getUserAllFavouriteImages(userId: string) {
-      if (!this.session) return;
+    async getUserAllFavouriteImages(userId: string): Promise<IFavourite[]> {
+      if (!this.session) return [];
 
       const { data, error } = await supabase
         .from('favourite')
         .select('*')
         .eq('user_id', userId)
 
-      if (data && !error) this.favourites.push(...data as any);
+      if (data && !error) return data;
+      return [];
     },
 
     async favouriteImage(isoDate: string, targetState: boolean) {
@@ -42,7 +44,7 @@ export const useAppStore = defineStore('app', {
           .insert({ image_date: isoDate, user_id: this.session.user.id })
           .select()
 
-        if (data && !error) this.favourites.push(...data as any);
+        if (data && !error) this.favourites.push(...data);
       }
       else {
         const { error } = await supabase
@@ -53,6 +55,27 @@ export const useAppStore = defineStore('app', {
 
         if (!error) this.favourites = this.favourites.filter(f => f.image_date !== isoDate)
       }
+    },
+
+    async createUser() {
+      if (!this.session) return;
+
+      const { data, error } = await supabase
+        .from('user')
+        .upsert({ id: this.session.user.id })
+        .select()
+
+      if (data && !error) this.user = data[0];
+    },
+
+    async getUser(userId: string): Promise<IUser | undefined> {
+      const { data, error } = await supabase
+        .from('user')
+        .select('*')
+        .eq("id", userId)
+
+      if (data && !error) return data[0];
+      return undefined;
     },
   }
 })
