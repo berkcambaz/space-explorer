c75ccfc3a96dbeed98d8d4ad9b5148fca13a736f
Berk Cambaz <berkcambaz12321@gmail.com>
2023-08-14 14:18:49 +0300

    Add plugin to minify html code on build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { createHtmlPlugin as html } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({ template: { transformAssetUrls } }),
     vuetify({ autoImport: true }),
+    html({ minify: true }),
   ],
   resolve: {
     alias: {
